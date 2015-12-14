9adbeb4281db94e22290e37dabd53bbd70766144
Julien Leal <julienbreno.nave@gmail.com>
2015-12-13 22:37:24 -0200

    TODO object array commited

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var express = require("express");
 app = express();
 var PORT = process.env.PORT||3000;
+var todos =[{
+    id:1,
+    description:'Meet mom for lunch.',
+    completed:false
+},{
+    id:2 ,
+    description: 'Go to supermarket',
+    completed:false
+}];
+
 app.get('/',function(req,res){
     res.send('TODO api Root');
 });
+
+app.get('/todos',function(req,res){
+   res.json(todos);
+});
+
 
 app.listen(PORT,function(){
    console.log('Express listening port :'+PORT);
