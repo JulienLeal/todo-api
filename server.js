71517f8ca74c5cd4bafa3455cb4a322f3fccead3
Julien Leal <julienbreno.nave@gmail.com>
2015-12-13 23:03:11 -0200

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var todos =[{
     id:2 ,
     description: 'Go to supermarket',
     completed:false
+},{
+    id:3 ,
+    description: 'Feed the cat',
+    completed:true
 }];
 
 app.get('/',function(req,res){
@@ -19,6 +23,25 @@ app.get('/todos',function(req,res){
    res.json(todos);
 });
 
+app.get('/todos/:id',function(req,res){
+    var todoID=parseInt(req.params.id,10);
+    var matchedTODO;
+
+    //iterate of todos array. find the match
+    todos.forEach(function(todo){
+        if(todoID===todo.id){
+            matchedTODO=todo;
+        }
+    });
+
+    if(matchedTODO){
+        res.json(matchedTODO);
+    }else{
+        res.status(404).send();
+    }
+
+   res.send('Asking for TODO with id of :' + req.params.id);
+});
 
 app.listen(PORT,function(){
    console.log('Express listening port :'+PORT);
