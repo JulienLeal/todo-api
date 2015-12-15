00b0b1fca4ddeb8303eecaf73be540aa4699a234
Julien Leal <julienbreno.nave@gmail.com>
2015-12-15 02:16:07 -0200

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -50,6 +50,47 @@ app.post('/todos',function(req,res){
 
 });
 
+app.delete('/todos/:id',function(req,res){
+    var todoID=parseInt(req.params.id,10);
+   var matchedTODO= _.findWhere(todos,{id:todoID});
+
+    if(!matchedTODO){
+        res.status(404).json({"error":"no todo found with that id"});
+    }else{
+        todos = _.without(todos, matchedTODO);
+        res.json(matchedTODO);
+    }
+
+});
+
+app.put('/todos/:id',function(req,res){
+    var todoID=parseInt(req.params.id,10);
+    var matchedTODO= _.findWhere(todos,{id:todoID});
+    var body = _.pick(req.body,'description','completed');
+    var validAttributes={};
+
+    if(!matchedTODO){
+        return res.status(404).send();
+    }
+
+    if(body.hasOwnProperty('completed')&& _.isBoolean(body.completed))
+    {
+        validAttributes.completed=body.completed;
+    }else if(body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    if(body.hasOwnProperty('description')&& _.isString(body.description) && body.description.trim().length>0)
+    {
+        validAttributes.description=body.description;
+    }else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTODO,validAttributes);
+    res.json(matchedTODO);
+
+});
+
 app.listen(PORT,function(){
    console.log('Express listening port :'+PORT);
 });
