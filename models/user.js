500176b17536218e8bb8a02427eb946210d32d39
Julien Leal <julienbreno.nave@gmail.com>
2015-12-23 16:41:20 -0200

    Resolve ._

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 var bcrypt = require('bcrypt');
 var _ =require('underscore');
+var cryptjs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports=function(sequelize, DataTypes) {
-    var user= sequelize.define('user', {
+    var user    = sequelize.define('user', {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
@@ -72,6 +74,23 @@ module.exports=function(sequelize, DataTypes) {
             toPublicJSON:function(){
                 var json = this.toJSON();
                 return _.pick(json,'id','email','createdAt','updatedAt');
+            },
+            generateToken:function(type){
+                if(!_.isString(type))
+                {
+                    return undefined;
+                }
+                try{
+                    var stringData = JSON.stringify({id:this.get('id'),type:type});
+                    var encryptedData = cryptjs.AES.encrypt(stringData,'abc123!@!').toString();
+                    var token = jwt.sign({
+                        token:encryptedData
+                    }, 'qwerty098');
+                    return token;
+                }catch(e){
+                    console.error(e);
+                    return undefined;
+                }
             }
         }
     });
